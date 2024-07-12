48020e99ae60862ad5bf3149ab5bd531fda31499
Thomas Cottis <=>
2024-07-12 13:37:16 -0600

    condiments hero done

--- src/components/CondimentHero.comp.jsx
+++ src/components/CondimentHero.comp.jsx
@@ -1,8 +1,19 @@
+import explosion from "../assets/explosion.png";
+
 const CondimentHero = () => {
     return (
-        <>
-            <h1>THIS IS THE CONDIMENT HERO</h1>
-        </>
+        <section id="condimentHero">
+            <div>
+                <h1 className="title">
+                    We Stock Booming Flavor
+                </h1>
+                <p className="subtitle">
+                    Check out the name brand condiments and spices that we have in stock!
+                </p>
+                <button>Explore Spices and Condiments</button>
+                <img src={explosion}/>
+            </div>
+        </section>
     );
 }
 
