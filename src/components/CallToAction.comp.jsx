f07c8c5d6f277a5e15080d6a84fb85115845f00a
Thomas Cottis <=>
2024-07-12 01:07:01 -0600

    call to action is simple, big, next is condiments

--- src/components/CallToAction.comp.jsx
+++ src/components/CallToAction.comp.jsx
@@ -1,9 +1,13 @@
 const CallToAction = () => {
     return (
-        <>
-            <h1>THIS IS THE CALL TO ACTION</h1>
-        </>
-    );
+			<section id="callToAction">
+				<div>
+					<h1>Make every meal memorable</h1>
+					<p className="subtitle">Choose our premium cuts!</p>
+                    <button>Order Now!</button>
+				</div>
+			</section>
+		);
 }
 
 export default CallToAction;
