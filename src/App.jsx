488658a7a5b168cff2f5fa22a5253af02c233d2b
Thomas Cottis <=>
2024-07-15 15:02:39 -0600

    implemented basic not found page, will search for inspiration later

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.page";
+import NotFound from "./pages/NotFound.page";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="*" element={<NotFound />}/>
       </Routes>
     </Router>
   );
