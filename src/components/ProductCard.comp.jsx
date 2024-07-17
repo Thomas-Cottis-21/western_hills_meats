d50a67b058e6a4e3469acc62da3447bee6a64279
Thomas Cottis <=>
2024-07-17 15:44:04 -0600

    styled product lists, next is to test styling, add images

--- src/components/ProductCard.comp.jsx
+++ src/components/ProductCard.comp.jsx
@@ -1,16 +1,14 @@
 const ProductCard = ({ product }) => {
     return (
 			<div className="productCard">
-				<span>{product.cloverId}</span>
-				<span>{product.name}</span>
-				<span>
-					{product.price} / {product.priceTypeName}
-				</span>
-				<span>{product.description}</span>
-				<span>
+				<p className="title">{product.name}</p>
+				<p className="productSupport">
+					<span>{product.price}</span> / {product.priceTypeName}
+				</p>
+				<p className="productSupport">
 					{product.categoryName} / {product.subcategoryName}
-				</span>
-                <span>{product.brandName}</span>
+				</p>
+				<p className="productSupport">{product.brandName}</p>
 			</div>
 		);
 }
--- src/components/ProductGrid.comp.jsx
+++ src/components/ProductGrid.comp.jsx
@@ -4,8 +4,8 @@ const ProductGrid = (props) => {
     return (
         <div className="productGrid">
             {props.data && props.data.length > 0 ? (
-                props.data.map(product => (
-                    <ProductCard key={product.cloverId} product={product}/>
+                props.data.map((product, index) => (
+                    <ProductCard key={index} product={product}/>
                 ))
             ) : (
                 <h1>No data</h1>
