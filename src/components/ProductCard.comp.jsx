c92413b336ada7b9ee33745ea9ace53e7c91ad9b
Thomas Cottis <=>
2024-07-19 11:20:46 -0600

    styled product cards with images, next is to upload product images to space + query images, then style meat product cards differently

--- src/components/ProductCard.comp.jsx
+++ src/components/ProductCard.comp.jsx
@@ -1,24 +1,53 @@
 import { formatStockClass } from "../util/formatString.util";
+import productPhoto from "../assets/productPhoto.jpg";
 
 const ProductCard = ({ product }) => {
-    return (
-			<div className="productCard">
-				<p className="title">{product.name}</p>
-				<p className="productSupport">
-					<span>${product.price}</span> / {product.priceTypeName}
-				</p>
-				<p className="productSupport">
-					{product.categoryName} / {product.subcategoryName}
-				</p>
-				<p
-					className={`productSupport ${formatStockClass(
-						product.quantityTypeName
-					)}`}>
-					{product.quantityTypeName}
-				</p>
-				<p className="productSupport">{product.brandName}</p>
-			</div>
-		);
-}
+	return (
+		<>
+			{product.categoryName === "Meats" ? (
+				<div className="productCard">
+					<div className="productListingData">
+						<p className="title">{product.name}</p>
+						<p className="productSupport">
+							<span>${product.price}</span> / {product.priceTypeName}
+						</p>
+						<p className="productSupport">
+							{product.categoryName} / {product.subcategoryName}
+						</p>
+						<p
+							className={`productSupport ${formatStockClass(
+								product.quantityTypeName
+							)}`}>
+							{product.quantityTypeName}
+						</p>
+						<p className="productSupport">{product.brandName}</p>
+					</div>
+				</div>
+			) : (
+				<div className="productCard">
+					<div className="image">
+						<img src={productPhoto} />
+					</div>
+					<div className="productListingData">
+						<p className="title">{product.name}</p>
+						<p className="productSupport">
+							<span>${product.price}</span> / {product.priceTypeName}
+						</p>
+						<p className="productSupport">
+							{product.categoryName} / {product.subcategoryName}
+						</p>
+						<p
+							className={`productSupport ${formatStockClass(
+								product.quantityTypeName
+							)}`}>
+							{product.quantityTypeName}
+						</p>
+						<p className="productSupport">{product.brandName}</p>
+					</div>
+				</div>
+			)}
+		</>
+	);
+};
 
 export default ProductCard;
