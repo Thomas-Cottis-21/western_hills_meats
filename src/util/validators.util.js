21449d8ab1f69878fce4e6868aea95284585f51a
Thomas Cottis <=>
2024-07-23 14:11:58 -0600

    validating email in frontend

--- src/util/validators.util.js
+++ src/util/validators.util.js
@@ -6,6 +6,8 @@ export const validateName = (value) => {
     if (value.trim().length > 50) {
         return `Name field cannot exceed 50 characters (${value.trim().length})`;
     }
+
+    return null;
 }
 
 export const validateEmail = (value) => {
@@ -16,6 +18,14 @@ export const validateEmail = (value) => {
     if (value.trim().length > 254) {
         return `Email field cannot exceed 254 characters (${value.trim().length})`;
     }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(value)) {
+        return "Please submit valid email address";
+    }
+
+    return null;
 }
 
 export const validateMessage = (value) => {
@@ -26,4 +36,6 @@ export const validateMessage = (value) => {
     if (value.trim().length > 5000) {
         return `Message field cannot exceed 5000 characters (${value.trim().length})`;
     }
+
+    return null;
 }
