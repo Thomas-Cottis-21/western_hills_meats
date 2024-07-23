4077a06ec61068260a01c96c379bc1e4b4f30b8e
Thomas Cottis <=>
2024-07-23 14:03:04 -0600

    contact modal is working as expected, next is to fix cors errors or launch to digital ocean

--- src/components/ContactModal.comp.jsx
+++ src/components/ContactModal.comp.jsx
@@ -1,14 +1,24 @@
 import { Button, Modal } from "react-bootstrap";
 
-const ContactModal = ({ show, handleClose }) => {
+const ContactModal = ({ show, handleClose, success }) => {
+
+    
+
     return (
         <>
-            <Modal show={show} onHide={handleClose}>
+            <Modal 
+            className={success ? "success" : "error"} 
+            show={show} 
+            onHide={handleClose}
+            centered>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal Heading</Modal.Title>
+                    <Modal.Title>{success ? "Message Sent!" : "Error, message not sent"}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    This is the modal body
+                    {success
+                        ? "Your message was sent successfully! Please allow 24 hours for us to reply" 
+                        : 
+                        "An error occurred while sending your message. Please try again later or call us at +1 385 899 8586"}
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>Close</Button>
