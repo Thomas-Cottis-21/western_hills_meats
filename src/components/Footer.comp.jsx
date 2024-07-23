aae7beff12a1798fc85019f96f14e8c20a97f088
Thomas Cottis <=>
2024-07-23 10:10:23 -0600

    added footer padding + store hours, next is form

--- src/components/Footer.comp.jsx
+++ src/components/Footer.comp.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 				<div className="footerContainer">
 					<div className="footerLinksContainer">
 						<div className="footerLinks">
-							<h2>Quick links</h2>
+							<h2 className="light">Quick links</h2>
 							<Link to={"/"}>Home</Link>
                             <Link to={"/inventory"}>Inventory</Link>
                             <Link to={"/inventory/meats"}>Meats</Link>
@@ -14,7 +14,7 @@ const Footer = () => {
                             <Link to={"/inventory/smokers"}>Grills & Smokers</Link>
 						</div>
 						<div className="footerLinks">
-							<h2>Partners</h2>
+							<h2 className="light">Partners</h2>
 							<a
 								href="https://www.traeger.com/"
 								target="_blank"
@@ -48,11 +48,11 @@ const Footer = () => {
 						</div>
 					</div>
 					<div className="footerSupportContainer">
-						<h2>Support</h2>
-						<p className="subtitle">
+						<h2 className="light">Support</h2>
+						<p className="subtitle light">
 							Store Phone: <a href="tel:+13858998586">+1 385 899 8586</a>
 						</p>
-						<p className="subtitle">
+						<p className="subtitle light">
 							Store Address:{" "}
 							<a
 								href="https://www.google.com/maps/place/Western+Hills+Meats%2FBBQ+Pit+Stop/@40.0438898,-111.720504,17z/data=!3m1!4b1!4m6!3m5!1s0x874da565e0b89d15:0x8a9eeae05a44346f!8m2!3d40.0438898!4d-111.7179291!16s%2Fg%2F11ss5b_t99?entry=ttu"
@@ -60,6 +60,9 @@ const Footer = () => {
 								rel="noopener noreferrer">
 								35 N 900 E St, Payson, UT 84651
 							</a>
+						</p>
+						<p className="subtitle light">
+							Store Hours: Monday - Saturday 10AM - 6PM
 						</p>
 					</div>
 				</div>
