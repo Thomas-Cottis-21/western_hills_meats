ae0a32d032c90e3415be7e011835bf9b56009954
Thomas Cottis <=>
2024-07-23 11:20:32 -0600

    implemented more validation, next is to create endpoint on server, implement onsend method

--- src/util/validators.util.js
+++ src/util/validators.util.js
@@ -2,16 +2,28 @@ export const validateName = (value) => {
     if (value.trim().length === 0) {
         return "Name is required";
     }
+
+    if (value.trim().length > 50) {
+        return `Name field cannot exceed 50 characters (${value.trim().length})`;
+    }
 }
 
 export const validateEmail = (value) => {
     if (value.trim().length === 0) {
         return "Email is required";
     }
+
+    if (value.trim().length > 254) {
+        return `Email field cannot exceed 254 characters (${value.trim().length})`;
+    }
 }
 
 export const validateMessage = (value) => {
     if (value.trim().length === 0) {
         return "Message is required";
+    }
+
+    if (value.trim().length > 5000) {
+        return `Message field cannot exceed 5000 characters (${value.trim().length})`;
     }
 }
