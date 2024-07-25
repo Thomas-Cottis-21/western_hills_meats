19b37af9b00b0f534cf8f1c407682702778eca7e
Thomas Cottis <=>
2024-07-25 13:20:46 -0600

    added styling to not found 404 page, next is to test site on different browesers

--- src/pages/NotFound.page.jsx
+++ src/pages/NotFound.page.jsx
@@ -1,10 +1,11 @@
-import Footer from "../components/Footer.comp";
-
 const NotFound = () => {
     return (
-        <>
-            <h1>Not Found lol</h1>
-        </>
+        <section className="notFound">
+            <h1 className="light">Looks like this page got grilled...</h1>
+            <p className="subtitle light">
+                Click one of the links below or use the navigation bar above to return to our website!
+            </p>
+        </section>
     );
 }
 
