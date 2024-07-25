42c77f14eb1f94f9e46448d3e19469cce5ba2bd7
Thomas Cottis <=>
2024-07-25 14:25:05 -0600

    installed helmet for SEO / metadata in react, next is to configure helmet for each page

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -4,16 +4,20 @@ import Contact from "../components/Contact.comp.jsx";
 import Hero from "../components/Hero.comp.jsx";
 import MeatHero from "../components/MeatHero.comp.jsx";
 import SmokerHero from "../components/SmokerHero.comp.jsx";
-import TopBar from "../components/TopBar.comp.jsx";
 import Vendor from "../components/Vendor.comp.jsx";
 import Location from "../components/Location.comp.jsx";
-import Navigation from "../components/Navigation.comp.jsx";
-import Footer from "../components/Footer.comp.jsx";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
 
     return (
         <>
+            <Helmet>
+                <title>Home</title>
+                <meta name="description" content="Visit us in Payson, Utah! We supply the best meats, rubs, grills and smokers in town!"/>
+                <meta name="keywords" content="meat, meats, beef, pork, chicken, lamb, turkey, seafood, condiments, rubs, sauces, marinades, accessories, thermometers, pellets, grills, smokers"/>
+                <link rel="canonical" href="https://www.ahiporti.com/"/>
+            </Helmet>
             <Hero/>
             <MeatHero/>
             <Vendor />
